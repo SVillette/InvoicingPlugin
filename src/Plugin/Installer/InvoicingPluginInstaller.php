a1aea6e231fcae2760dcf842aa1fd0f6ddb4a5b7
Rafikooo <thecrazyrafik@gmail.com>
2025-05-20 11:13:04 +0200

    WIP

--- src/Plugin/Installer/InvoicingPluginInstaller.php
+++ src/Plugin/Installer/InvoicingPluginInstaller.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Sylius\InvoicingPlugin\Plugin\Installer;
 
 use App\Plugin\Installer\PluginInstallerInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\DependencyInjection\Attribute\AsTaggedItem;
 use Symfony\Component\Process\Process;
 
@@ -16,8 +17,10 @@ class InvoicingPluginInstaller implements PluginInstallerInterface
         return $packageName === 'sylius/invoicing-plugin';
     }
 
-    public function install(string $version): void
+    public function install(SymfonyStyle $io): void
     {
+        $io->title('Installing Invoicing Plugin');
+
         Process::fromShellCommandline(
             "grep -Fq 'pdf_generator:' config/packages/sylius_invoicing.yaml || " .
             "cat << 'EOF' >> config/packages/sylius_invoicing.yaml\n" .
@@ -26,5 +29,16 @@ class InvoicingPluginInstaller implements PluginInstallerInterface
             "        enabled: false\n" .
             "EOF"
         )->run();
+    }
+
+    public function finalize(SymfonyStyle $io): void
+    {
+        $io->title('Finalizing Invoicing Plugin installation');
+
+        $io->text(
+            Process::fromShellCommandline('php bin/console doctrine:schema:update --force --complete')
+                ->mustRun()
+                ->getOutput()
+        );
     }
 }
